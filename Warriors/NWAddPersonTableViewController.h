fcf666204815b582a22013eb34c9e29b30225017
kumok <bhh@byu.edu>
2015-03-18 20:18:05 -0600

    nowhere, before handling input

---
+++ Warriors/NWAddPersonTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  NWAddPersonTableViewController.h
+//  Warriors
+//
+//  Created by Duc Ho on 3/18/15.
+//  Copyright (c) 2015 brianhollc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Event.h"
+
+@interface NWAddPersonTableViewController : UITableViewController
+
+@property (strong, nonatomic) Event * event;
+
+@end
