2b5d69abb410b52888ac957ac96cbcd1eb5cc22e
kumok <bhh@byu.edu>
2015-03-21 00:58:05 -0600

    dropbox works

--- Warriors/AppDelegate.m
+++ Warriors/AppDelegate.m
@@ -10,6 +10,7 @@
 #import "Person.h"
 #import "Event.h"
 #import "MagicalRecord.h"
+#import <DropboxSDK/DropboxSDK.h>
 
 @interface AppDelegate ()
 
@@ -90,6 +91,20 @@
 
     return YES;
 }
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
+  sourceApplication:(NSString *)source annotation:(id)annotation {
+    if ([[DBSession sharedSession] handleOpenURL:url]) {
+        if ([[DBSession sharedSession] isLinked]) {
+            NSLog(@"App linked successfully!");
+            // At this point you can start making API calls
+        }
+        return YES;
+    }
+    // Add whatever other url handling code your app requires here
+    return NO;
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ Warriors/NWDropboxSyncViewController.m
@@ -0,0 +1,91 @@
+//
+//  NWDropboxSyncViewController.m
+//  Warriors
+//
+//  Created by Duc Ho on 3/20/15.
+//  Copyright (c) 2015 brianhollc. All rights reserved.
+//
+
+#import "NWDropboxSyncViewController.h"
+#import <DropboxSDK/DropboxSDK.h>
+
+
+@interface NWDropboxSyncViewController () <DBRestClientDelegate>
+@property (nonatomic, strong) DBRestClient *restClient;
+
+@end
+
+@implementation NWDropboxSyncViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    DBSession *dbSession = [[DBSession alloc]
+                            initWithAppKey:@"cuiu39svaraqyc4"
+                            appSecret:@"v0ate5scmnb0h5o"
+                            root:kDBRootAppFolder]; // either kDBRootAppFolder or kDBRootDropbox
+    [DBSession setSharedSession:dbSession];
+    
+    self.restClient = [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
+    self.restClient.delegate = self;
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)syncWithDropbx:(id)sender {
+    if (![[DBSession sharedSession] isLinked]) {
+        [[DBSession sharedSession] linkFromController:self];
+    }
+    
+    NSString *text = @"0306703,0035866,NO_ACTION,06/19/2006\n0086003,\"0005866\",UPDATED,06/19/2007";
+    NSString *filename = @"NWData.csv";
+    NSString *localDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    NSString *localPath = [localDir stringByAppendingPathComponent:filename];
+    [text writeToFile:localPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    // Upload file to Dropbox
+    NSString *destDir = @"/";
+//    [self.restClient uploadFile:filename toPath:destDir withParentRev:nil fromPath:localPath];
+    NSString *parentRev= [[NSUserDefaults standardUserDefaults] objectForKey:@"DBParentRev"];
+    if (!parentRev) {
+        [self.restClient uploadFile:filename toPath:destDir withParentRev:nil fromPath:localPath];
+    }
+    else
+    {
+        [self.restClient uploadFile:filename toPath:destDir withParentRev:parentRev fromPath:localPath];
+    }
+
+
+    
+}
+
+- (void)restClient:(DBRestClient *)client uploadedFile:(NSString *)destPath
+              from:(NSString *)srcPath metadata:(DBMetadata *)metadata {
+    NSLog(@"File uploaded successfully to path: %@", metadata.path);
+    NSLog(@"%@",metadata.rev);
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:metadata.rev forKey:@"DBParentRev"];
+    [defaults synchronize];
+
+    
+}
+
+- (void)restClient:(DBRestClient *)client uploadFileFailedWithError:(NSError *)error {
+    NSLog(@"File upload failed with error: %@", error);
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
